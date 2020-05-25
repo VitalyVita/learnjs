a7f6492b9d8931228254719b90bc4e2562f5d465
vitaly <klibishevvvv@gmail.com>
2020-05-25 10:12:26 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+//const result = confirm("Are you here?");
+//const answer = prompt ("Вам есть 18?", "Да, мне есть 18");
+//console.log (answer);
+
+
+const answers = [];
+
+answers[0] = prompt ('Как ваше имя?','')
+answers[1] = prompt ('Сколько вам лет?','')
+answers[2] = prompt ('Ваш город проживания?','')
+
+
+document.write(answers);
